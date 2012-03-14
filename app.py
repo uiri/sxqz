e75e0f000d8844c3c34ece4af2a141570a70a09f
Uiri <uiri00@gmail.com>
2012-03-13 23:52:48 -0400

    forgot to bind to port

--- app.py
+++ app.py
@@ -7,3 +7,8 @@ def index():
     returnfile = open("home.tpl", 'r')
     returnstr = returnfile.read()
     return returnstr
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
