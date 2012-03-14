c1d42f2b149855b7e554077f898459a0802b9f92
Uiri <uiri00@gmail.com>
2012-03-14 02:06:59 -0400

    It isn't hard to rewrite this stuff I guess...

--- app.py
+++ app.py
@@ -13,9 +13,47 @@ def index():
 @app.route('/search')
 def search():
     query = urllib.unquote_plus(request.args.get('q', ''))
+    query = query.replace(' //', '//')
     if re.search("^=", query):
         wolframalpha = "http://wolframalpha/input/?i=" + urllib.quote_plus(query[1:])
         return redirect(wolframalpha)
+    elif re.search("//", query):
+        #if re.search("/[A-Za-z]$", query):
+            #stuff
+        #if re.search("//define$", query):
+            #stuff
+        if re.search("//map$", query):
+            query = query.replace("//map", "")
+            mapurl = "http://google.com/maps?q=" + urllib.quote_plus(query)
+            return redirect(mapurl)
+        elif re.search("//img$", query):
+            query = query.replace("//img", "")
+            imgurl = "http://google.com/search?tbm=isch&q=" + urllib.quote_plus(query)
+            return redirect(imgurl)
+        elif re.search("//code(;l=[A-Za-z]+)?$", query):
+            if re.search("//code$/", query):
+                params = ""
+            else:
+                paramslist = re.findall("//code;l=([A-Za-z]+)$", query)
+                params = "&Language=" + paramslist[0][1]
+            query = query.split("//")[0]
+            codeurl = "http://github.com/search?type=Code" + params + "&q=" + query
+            return redirect(codeurl)
+        else:
+            if not re.search("\.[A-Za-z]{2,4}$", query):
+                if re.search("\.o$", query):
+                    query += "rg"
+                elif re.search("\.n$", query):
+                    query += "et"
+                elif re.search("\.$", query):
+                    query += "com"
+                else:
+                    query += ".com"
+            if re.search("^//", query):
+                newurl = "http:" + query
+                return redirect(newurl)
+            else:
+                query = query.replace("//", " site:")
     else:
         wikiurl = "https://en.wikipedia.org/w/api.php?format=json&action=query&titles=" + query
         #request = urllib2.Request(url=wikiurl, headers={'User-Agent': 'Pass-Thrust'})
@@ -24,9 +62,8 @@ def search():
         if int(res[u'query'][u'pages'].keys()[0]) != -1:
             wikipedia = "http://en.wikipedia.org/wiki/" + urllib.quote_plus(query)
             return redirect(wikipedia)
-        else:
-            bing = "http://bing.com/search?q=" + urllib.quote_plus(query)
-            return redirect(bing)
+    bing = "http://bing.com/search?q=" + urllib.quote_plus(query)
+    return redirect(bing)
     #return "hi"
 
 if __name__ == '__main__':
